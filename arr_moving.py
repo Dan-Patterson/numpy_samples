738fea94dc65ee386605120a6688707a5bfd40e9
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2016-08-31 04:41:35 -0400

    Update arr_moving.py
    general format editing

--- arr_moving.py
+++ arr_moving.py
@@ -16,7 +16,7 @@
 :  ---- rolling_stats example ----
 :  Stride the input array, 'a' using a 3x3 window
 :
-:    c = stride(a, r_c=(3,3))
+:    c = stride(a, r_c=(3, 3))
 :
 :    Main array... 'a'
 :    ndim: 2 size: 54
@@ -123,13 +123,14 @@ from arr_tools import deline
 __all__ = ['_check', 'block', 'stride', 'rolling_stats']
 __outside__ = ['as_strided', 'dedent', 'deline']
 
-ft={'bool':lambda x: repr(x.astype('int32')),
+ft={'bool': lambda x: repr(x.astype('int32')),
     'float': '{: 0.1f}'.format}
 np.set_printoptions(edgeitems=5, linewidth=80, precision=2,
                     suppress=True, threshold=100, 
                     formatter=ft)
 
 script = sys.argv[0]
+
 
 #---- functions ----
 def _check(a, r_c):
@@ -140,7 +141,7 @@ def _check(a, r_c):
     :     minimum shape will be (1*r) for 1D array or (1*c) for 2D
     :     array if r<c.  Be aware
     """
-    if isinstance(r_c,(int, float)):
+    if isinstance(r_c, (int, float)):
         r_c = (1, int(r_c))
     r, c = r_c
     a = np.atleast_2d(a)
@@ -149,12 +150,13 @@ def _check(a, r_c):
     a = np.ascontiguousarray(a)
     return a, shp, r, c, tuple(r_c)
 
+
 def _pad(a, nan_edge=False):
     """Pad a sliding array to allow for stats"""
     if nan_edge:
-        a = np.pad(a, pad_width=(1,2), mode="constant", constant_values=(np.NaN,np.NaN))
+        a = np.pad(a, pad_width=(1,2), mode="constant", constant_values=(np.NaN, np.NaN))
     else:
-        a = np.pad(a, pad_width=(1,1), mode="reflect") #reflect
+        a = np.pad(a, pad_width=(1,1), mode="reflect")  # reflect
     return a
         
 def stride(a, r_c=(3,3)):
@@ -207,25 +209,28 @@ def rolling_stats(a, no_null=True, prn=True):
     a = np.asarray(a)
     a = np.atleast_2d(a)
     if a.ndim > 1:
-        ax = (2,3) # ax = tuple(np.arange(len(c.shape))[-2:])
+        ax = (2, 3) # ax = tuple(np.arange(len(c.shape))[-2:])
     if no_null:
-        a_min = a.min(axis=ax);  a_max = a.max(axis=ax)
-        a_mean = a.mean(axis=ax);  a_sum = a.sum(axis=ax)
-        a_std = a.std(axis=ax);  a_var = a.var(axis=ax)
+        a_min = a.min(axis=ax)
+        a_max = a.max(axis=ax)
+        a_mean = a.mean(axis=ax)
+        a_sum = a.sum(axis=ax)
+        a_std = a.std(axis=ax)
+        a_var = a.var(axis=ax)
         a_ptp = a_max - a_min
     else:
-        a_min = np.nanmin(a,axis=(ax))
-        a_max = np.nanmax(a,axis=(ax))
-        a_mean = np.nanmean(a,axis=(ax))
-        a_sum = np.nansum(a,axis=(ax))
-        a_std = np.nanstd(a,axis=(ax))
-        a_var = np.nanvar(a,axis=(ax))
+        a_min = np.nanmin(a, axis=(ax))
+        a_max = np.nanmax(a, axis=(ax))
+        a_mean = np.nanmean(a, axis=(ax))
+        a_sum = np.nansum(a, axis=(ax))
+        a_std = np.nanstd(a, axis=(ax))
+        a_var = np.nanvar(a, axis=(ax))
         a_ptp = a_max - a_min
     if prn:
         frmt = "Minimum...\n{}\nMaximum...\n{}\nMean...\n{}\n" +\
                "Sum...\n{}\nStd...\n{}\nVar...\n{}\nRange...\n{}"
         frmt = dedent(frmt)
-        args = [a_min,a_max,a_mean, a_sum, a_std, a_var, a_ptp]
+        args = [a_min, a_max, a_mean, a_sum, a_std, a_var, a_ptp]
         print(frmt.format(*args))
     else:
         return a_min, a_max, a_mean, a_sum, a_std, a_var, a_ptp
@@ -238,8 +243,8 @@ def _demo():
     """
     r = 6        # array rows
     c = 9        # array columns
-    r_c = (3,3)  # moving/block window size
-    a = np.arange(r*c).reshape(r,c)
+    r_c = (3, 3)  # moving/block window size
+    a = np.arange(r*c).reshape(r, c)
     b = block(a)
     c = stride(a, r_c)
     deline(a, prn=True)
@@ -253,13 +258,13 @@ def _demo():
     {}
     :etc......
     :rolling mean:
-    :  c = stride(a, r_c=(3,3))
+    :  c = stride(a, r_c=(3, 3))
     :  c.shape  # (4, 7, 3, 3)
-    :  ax = tuple(np.arange(len(c.shape))[-2:]) #(0,1,2,3) => (2,3)
+    :  ax = tuple(np.arange(len(c.shape))[-2:])  # (0, 1, 2, 3) => (2, 3)
     :  c_m =np.mean(c, ax)
     : ==> {}
     """
-    c_m = np.mean(c,axis=(2,3))
+    c_m = np.mean(c,axis=(2, 3))
     as0 = deline(c[0][:2], prn=False)
     as1 = deline(c_m, prn=False)
     args = [c.ndim, c.size, as0, as1]
@@ -267,8 +272,8 @@ def _demo():
     return a, b, c
 
 #
-#----------------------
-if __name__=="__main__":
+#------------------------
+if __name__ == "__main__":
     """   """
     #print("Script... {}".format(script))
     a, b, c = _demo()
