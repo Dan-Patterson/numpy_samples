1f7a07a1fe7195833f8a98abac33c9ba443c5b6d
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2017-01-03 18:09:11 -0500

    Add files via upload
    Minor edits and cleanup.

--- geometry/circle_make.py
+++ geometry/circle_make.py
@@ -3,30 +3,13 @@
 :Script:   circle_make.py
 :Author:   Dan.Patterson@carleton.ca
 :Modified: 2017-01-02
-:Purpose:
-:-------
-: - Make a circle or an arc
-: - To form a polygon of an arc sector
-:  top = _arc(radius=10, start=0, stop=1, step=0.2, xc=0.0, yc=0.0)
-:  top.reverse()
-:  bott = _arc(radius=9.5, start=0, stop=1, step=0.2, xc=0.0, yc=0.0)
-:  top = np.array(top)
-:  bott = np.array(bott)
-:  close = top[0]
-:  arc = np.asarray([i for i in [*top, *bott, close]])
-:  pnts = arc.tolist()
-:  pnts
-:   [[9.99902524, 0.1396218], [9.99945169, 0.10471784],
-:    [9.99975631, 0.0698126], [9.99993908, 0.03490651],
-:    [10.0, 0.0], [9.5, 0.0], [9.49994212, 0.03316119],
-:    [9.49976849, 0.06632197], [9.49947911, 0.09948195],
-:    [9.49907398, 0.13264071], [9.99902524, 0.1396218]]
+:Purpose:  See the documentation for the functions
 :Notes:
 :
 :References
 :
 """
-#---- imports, formats, constants ----
+# ---- imports, formats, constants ----
 import sys
 import numpy as np
 from textwrap import dedent
@@ -44,7 +27,17 @@ np.set_printoptions(edgeitems=10, linewidth=80, precision=2,
 
 script = sys.argv[0]
 
+__all__ = ["plot_",
+           "rot_matrix",
+           "_arc",
+           "_circle",
+           "arc_sector",
+           "buffer_ring",
+           "multiring_buffer_demo",
+           "multi_sector_demo"
+           ]
 #---- functions ----
+
 def plot_(pnts):
     """plot a circle, arc sector etc
     """
@@ -78,7 +71,7 @@ def rot_matrix(angle=0, nm_3=False):
     :Requires:
     :--------
     :  - rotation angle in degrees and whether the matrix will be used with
-    :  - homogenous coordinates
+    :    homogenous coordinates
     :Returns:
     :-------
     :  - rot_m - rotation matrix for 2D transform
@@ -101,36 +94,37 @@ def _arc(radius=100, start=0, stop=1, step=0.1, xc=0.0, yc=0.0):
     :---------
     :  radius = cirle radius from which the arc is obtained
     :  start, stop, incr = angles in degrees
-    :  xc, yc - center coordinates in projected units
-    :  
+    :  xc, yc - center coordinates in projected units  
     :Returns: 
     :--------
     :  points on the arc
     """
     start, stop = sorted([start, stop])
     angle = np.deg2rad(np.arange(start, stop, step))
-    Xs = radius*np.cos(angle)         # X values
-    Ys = radius*np.sin(angle)         # Y values
-    pnts = np.array(list(zip(Xs, Ys)))
+    x_s = radius*np.cos(angle)         # X values
+    y_s = radius*np.sin(angle)         # Y values
+    pnts = np.c_[x_s, y_s]
     pnts = pnts + [xc, yc]
     p_lst = pnts.tolist()
     return p_lst
 
 
 def _circle(radius=100, clockwise=True, theta=1, rot=0.0, scale=1, xc=0.0, yc=0.0):
-    """   
+    """Produce a circle/ellipse depending on parameters.
     :Requires
     :--------
     :  radius - in projected units
     :  clockwise - True for clockwise (outer rings), 
     :            - False for counter-clockwise (for inner rings)
     :  theta -  angle spacing
-    :     If theta=1, angles between -180 to < 180, are returned in 1 degree
+    :     If theta=1, angles between -180 to 180, are returned in 1 degree
     :     increments. The endpoint is excluded.
-    :  
+    :  rot - rotation angle in degrees... used if scaling is not equal to 1
+    :  scale - for ellipses, change the scale to <1 or > 1. The resultant
+    :     y-values will favour the x or y-axis depending on the scaling.
     :Returns
     :-------
-    :  list of coordinates rounded to 3 decimal places
+    :  list of coordinates for the circle/ellipse
     :Notes:
     :------
     : You can also use np.linspace if you want to specify point numbers.
@@ -141,20 +135,32 @@ def _circle(radius=100, clockwise=True, theta=1, rot=0.0, scale=1, xc=0.0, yc=0.
         angles = np.deg2rad(np.arange(180.0, -180.0-theta, step=-theta))
     else:
         angles = np.deg2rad(np.arange(-180.0, 180.0+theta, step=theta))
-    #angles = np.append(angles, angles[0])
-    x_s = radius*np.cos(angles)         # X values
+    x_s = radius*np.cos(angles)            # X values
     y_s = radius*np.sin(angles) * scale    # Y values
-    pnts = np.c_[x_s,y_s]
+    pnts = np.c_[x_s, y_s]
     if rot != 0:
-        rot_mat = rot_matrix(angle=rot) #angle)
-        p_rot = np.dot(rot_mat, pnts.T) #p_trans.T).T + to_pnt
-        pnts = p_rot.T
+        rot_mat = rot_matrix(angle=rot)
+        pnts = (np.dot(rot_mat, pnts.T)).T
     pnts = pnts + [xc, yc]
     return pnts
 
 
 def arc_sector(outer=10, inner=9, start=1, stop=6, step=0.1):
-    """ """
+    """Form an arc sector bounded by a distance specified by two radii
+    : outer - outer radius of the arc sector
+    : inner - inner radius
+    : start - start angle of the arc
+    : stop - end angle of the arc
+    : step - the angle densification step
+    :Requires:
+    :--------
+    :  _arc - this def is used to produce the arcs, the top arc is rotated
+    :     clockwise and the bottom remains in the order produced to help
+    :     form closed-polygons.
+    """
+    s_s = [start, stop]
+    s_s.sort()
+    start, stop = s_s
     top = _arc(radius=outer, start=start, stop=stop, step=step, xc=0.0, yc=0.0)
     top.reverse()
     bott = _arc(radius=inner, start=start, stop=stop, step=step, xc=0.0, yc=0.0)
@@ -166,11 +172,27 @@ def arc_sector(outer=10, inner=9, start=1, stop=6, step=0.1):
     return pnts
 
 
-def buffer_ring(outer_rad=100, inner_rad=0, theta=10, rot=0, scale=1, xc=0.0, yc=0.0):
-    """Create a multi-ring buffer"""
-    top = _circle(outer_rad, clockwise=True, theta=theta, rot=rot, scale=scale, xc=xc, yc=yc)
-    if inner_rad != 0.0:
-        bott = _circle(inner_rad, clockwise=False, theta=theta, rot=rot, scale=scale, xc=xc, yc=yc)
+def buffer_ring(outer=100, inner=0, theta=10, rot=0, scale=1, xc=0.0, yc=0.0):
+    """Create a multi-ring buffer around a center point (xc, yc)
+    : outer - outer radius
+    : inner - inner radius
+    : theta - angles to use to densify the circle...
+    :    - 360+ for circle
+    :    - 120 for triangle
+    :    - 90  for square
+    :    - 72  for pentagon
+    :    - 60  for hexagon
+    :    - 45  for octagon
+    :    - etc
+    : rot - rotation angle, used for non-circles
+    : scale - used to scale the y-coordinates
+    :
+    """
+    top = _circle(outer, clockwise=True, theta=theta, rot=rot,
+                  scale=scale, xc=xc, yc=yc)
+    if inner != 0.0:
+        bott = _circle(inner, clockwise=False, theta=theta, rot=rot,
+                       scale=scale, xc=xc, yc=yc)
         pnts = np.asarray([i for i in [*top, *bott]])
     else:
         pnts = top
@@ -178,7 +200,9 @@ def buffer_ring(outer_rad=100, inner_rad=0, theta=10, rot=0, scale=1, xc=0.0, yc
     return pnts
 
 
-def multiring_buffer():
+# ---- demo functions ----
+
+def multiring_buffer_demo():
     """Do a multiring buffer
     : rads - buffer radii
     : theta - angle density... 1 for 360 ngon, 120 for triangle
@@ -186,28 +210,28 @@ def multiring_buffer():
     : scale - scale the y-values to produce ellipses
     """
     buffers = []
-    rads = [ 10, 20, 40, 80, 100] #, 40, 60, 100]
-    theta = 1
+    radii = [ 10, 20, 40, 80, 100] #, 40, 60, 100]
+    theta = 10
     rot = 22.5
     scale= 0.7
     clockwise=False
-    for r in range(0, len(rads)):
-        ring = buffer_ring(rads[r], rads[r-1], theta=theta, rot=rot, scale=scale)
+    for r in range(1, len(radii)):
+        ring = buffer_ring(radii[r], radii[r-1], theta=theta, rot=rot, scale=scale)
         buffers.append(ring)
     plot_(buffers)
     #return buffers
 
 
-def multi_sector():
-    """ """
+def multi_sector_demo():
+    """Produce multiple sectors  """
     sectors = []
     step = 5
     outer = 10
     inner = 9
     incr = np.arange(0, 91, 1) #(0,361,5)
     for outer in range(6, 10):
-        inner = outer -1
-        for i in range(1, len(incr)):
+        inner = outer - 1
+        for i in range(0, len(incr)):
             st = incr[i]
             end = incr[i-1]
             arc = arc_sector(outer=outer, inner=inner, start=st, stop=end, step=0.1)
@@ -220,31 +244,14 @@ def help_():
     print(__doc__)
 #----------------------
 if __name__=="__main__":
-    """   """
+    """Uncomment what you want to see"""
     #print("Script... {}".format(script))
-    #p_lst = _circle(radius=1, theta=30, xc=5, yc=5)
-    #print(p_lst)
-    #a_list = _arc(radius=10, start=0, stop=90.5, step=5, xc=0.0, yc=0.0)
-    #print("\narc points...\n{}".format(a_list))
+    #circ_pnts = _circle(radius=1, theta=30, xc=5, yc=5)
+    #print("\ncircle points...\n{}".format(circ_pnts))
+    #arc_pnts = _arc(radius=10, start=0, stop=90.5, step=5, xc=0.0, yc=0.0)
+    #print("\narc points...\n{}".format(arc_pnts))
     #pnts = arc_sector()
-    #pnts = buffer_rings()
-    #multi_sector()
-    multiring_buffer()
-"""
-rows = 4
-cols = 6
-N = 5    # your 51
-a = np.arange(rows*cols).reshape(rows, cols)
-b = np.arange(rows*cols).reshape(rows, cols) + 2
-c = np.zeros_like(b)
-print("Input arrays... a and b\n{}\n...\n{}".format(a, b))
-for x in range (1, N):
-    for i in range(0, 2):
-        for j in range(0, 2):
-            value1 = a[i, j]
-            value2 = b[i, j]
-            value1 = value1 * x
-            value3 = value1 + value2
-            c[i][j] = value3     # note change
-        print("c at {}, {}\n{}".format(i, j, c))
-"""
+    #pnts = buffer_ring()
+    #multi_sector_demo()
+    multiring_buffer_demo()
+
